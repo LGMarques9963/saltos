0f354439695a905f7a1984339071f886dbb3c50f
= <=>
2024-09-29 16:33:23 -0300

    Fix: calculo de f1, e removido o f0

--- main.cpp
+++ main.cpp
@@ -37,10 +37,6 @@ int f_rec(int n, string p, int tresMetros) {
 
 int f_memo(int n, string p, int tresMetros, map<string, int> &memo) {
     /**
-        A ideia é armazenar o resultado de f(n) em um dicionário, onde a chave é uma string composta 
-        por n e tresMetros concatenados. Se a chave já existe no dicionário,  então o valor correspondente 
-        é retornado, caso contrário, a função é chamada recursivamente e o resultado é armazenado no dicionário
-
         Input:
             n: tamanho da string p
             p: string que representa as posições das pedras
@@ -49,14 +45,37 @@ int f_memo(int n, string p, int tresMetros, map<string, int> &memo) {
         Output:
             quantidade de caminhos possíveis para chegar na posição n
 
+        A ideia é armazenar o resultado de f(n) em um dicionário, onde a chave é uma string composta 
+        por n e tresMetros concatenados. Se a chave já existe no dicionário,  então o valor correspondente 
+        é retornado, caso contrário, a função é chamada recursivamente e o resultado é armazenado no dicionário
+
+
      */
     string key = to_string(n) + to_string(tresMetros);
     if (n < 0) return 0;
     if (n == 0) return 1;
+    if (memo.find(key) != memo.end()) return memo[key]; 
     if (p[n] == '0') return 0;
-    if (memo.find(key) != memo.end()) return memo[key];
     if (tresMetros == 0) return memo[key] = f_memo(n - 1, p, 0, memo) + f_memo(n - 2, p, 0, memo) + f_memo(n - 3, p, 1, memo);
     if (tresMetros == 1) return memo[key] = f_memo(n - 1, p, 0, memo) + f_memo(n - 2, p, 0, memo);
+    
+    
+    
+    /**
+        Comentarios de Debug
+     */
+
+    // if (tresMetros == 0){
+    //     int res = f_memo(n - 1, p, 0, memo) + f_memo(n - 2, p, 0, memo) + f_memo(n - 3, p, 1, memo);
+    //     cout << "n: " << n << " p: " << p << " tresMetros: " << tresMetros << " res: " << res << endl;
+    //     return memo[key] = res;
+    // } 
+        
+    // if (tresMetros == 1) {
+    //     int res = f_memo(n - 1, p, 0, memo) + f_memo(n - 2, p, 0, memo);
+    //     cout << "n: " << n << " p: " << p << " tresMetros: " << tresMetros << " res: " << res << endl;
+    //     return memo[key] = res;
+    // }
     return 0;
 }
 
@@ -68,25 +87,21 @@ int f_iter(int n, string p) {
         Output:
             quantidade de caminhos possíveis para chegar na posição n
 
-        A ideia foi resolver o problema usando 3 vetores:
-            f0[n] é a quantidade de caminhos possíveis para chegar na posição n, considerando saltos de 1, 2 e 3 metros, 
+        A ideia foi resolver o problema usando 2 vetores:
             f1[n] é a quantidade de caminhos possíveis para chegar na posição n, desconsiderando os saltos de 3 metros
             fn[n] é a quantidade de caminhos possíveis para chegar na posição n, considerando a restrinção de não dar dois saltos de 3 metros seguidos
 
-        f0(n) = f0(n - 1) + f0(n - 2) + f0(n - 3) se p[n] != 0
-        f1(n) = f0(n - 1) + f0(n - 2) se p[n] != 0
         fn(n) = fn(n - 1) + fn(n - 2) + f1(n - 3) se p[n] != 0
+        f1(n) = fn(n - 1) + fn(n - 2) se p[n] != 0
         
      */
 
-    vector<int> f0(n + 1), f1(n + 1), fn(n + 1);
-    f0[0] = f1[0] = fn[0] = 1;
-    f0[1] = f1[1] = fn[1] = 1;
-    f0[2] = f1[2] = fn[2] = 2;
+    vector<int> f1(n + 1), fn(n + 1);
+    f1[0] = fn[0] = f1[1] = fn[1] = 1;
+    f1[2] = fn[2] = 2;
 
     for (int i = 3; i <= n; i++) {
-        f0[i] = p[i] == '0' ? 0 : f0[i - 1] + f0[i - 2] + f0[i - 3];
-        f1[i] = p[i] == '0' ? 0 : f0[i - 1] + f0[i - 2];
+        f1[i] = p[i] == '0' ? 0 : fn[i - 1] + fn[i - 2];
         fn[i] = p[i] == '0' ? 0 : fn[i - 1] + fn[i - 2] + f1[i - 3];
     }
     /**
@@ -95,10 +110,6 @@ int f_iter(int n, string p) {
 
      */
 
-    // cout << endl;
-    // for (int i = 0; i <= n; i++) {
-    //     cout << i << ":" << f0[i] << " ";
-    // }
     // cout << endl;
     // for (int i = 0; i <= n; i++) {
     //     cout <<  i << ":" << f1[i] << " ";
